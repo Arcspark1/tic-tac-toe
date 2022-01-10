3f5f7151edc64e4d1c4f68993a464482bd6248ad
Marc <marc.delongte@gmail.com>
2022-01-10 21:51:53 +0930

    Created grid styling

--- scripts.js
+++ scripts.js
@@ -1,25 +1,18 @@
-// const GameBoard = () => {
-//     const board = [
-//         ['x', 'x', 'o'],
-//         ['o', 'x', 'o'],
-//         ['x', 'o', 'o']
-//     ];
-//     const selection = (row, column) => board[row - 1][column - 1] = "x";
-//     return { board, selection }
-// }
-
+// Game Board using multi level array.
+//selection sets array to "x"
 const gameboard = (() => {
     const board = [
-        [],
-        [],
-        []
+        ["","",""],
+        ["","",""],
+        ["","",""]
     ];
 
     const selection = (row,column) => board[row - 1][column - 1] = "x";
     return {board, selection};
 })();
 
-gameboard.selection(2,1);
-gameboard.selection(2,2);
-gameboard.selection(2,3);
-console.log(gameboard.board);
+//Create Game Board HTML ELements
+
+const gameBoardElements = document.querySelector(".board");
+
+
